c87430e726b7268adbe34a10a7c793809647f5bb
shibinjohn88 <shibinjohnpdlm@gmail.com>
2023-01-11 06:23:12 -0500

    bootstrap styling added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Background from './components/Background';
+import StoreListings from './components/StoreListings';
+import TopBar from './components/TopBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar/>
+      <Background/>
+      <StoreListings/>
     </div>
   );
 }
